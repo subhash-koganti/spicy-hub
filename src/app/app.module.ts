16f08e7a922ce85a2f1ff031ed75fc753fbbc9d6
subhash-koganti <subhash.koganti@gmail.com>
2019-01-13 13:12:59 -0500

    Added some changes with styles images and carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { LeftFrameComponent } from './left-frame/left-frame.component';
 import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    LeftFrameComponent,
     MainPageComponent
   ],
   imports: [
